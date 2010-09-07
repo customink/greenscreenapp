34ce1cdde702f151c3a0fefd19ff27c4b6da844d
Rhett Sutphin <rhett@detailedbalance.net>
2010-09-07 09:29:05 -0500

    Whitespace

--- greenscreen.rb
+++ greenscreen.rb
@@ -8,13 +8,13 @@ require 'open-uri'
 get '/' do
   servers = YAML.load_file 'config.yml'
   return "Add the details of build server to the config.yml file to get started" unless servers
-  
+
   @projects = []
 
   servers.each do |server|
     xml = REXML::Document.new(open(server["url"], :http_basic_authentication=>[server["username"], server["password"]]))
     projects = xml.elements["//Projects"]
-    
+
     projects.each do |project|
       monitored_project = MonitoredProject.new(project)
       if server["jobs"]
@@ -31,7 +31,7 @@ get '/' do
   @columns = 2.0 if @projects.size > 4
   @columns = 3.0 if @projects.size > 10
   @columns = 4.0 if @projects.size > 21
-  
+
   @rows = (@projects.size / @columns).ceil
 
   erb :index
@@ -39,7 +39,7 @@ end
 
 class MonitoredProject
   attr_reader :name, :last_build_status, :activity, :last_build_time, :web_url, :last_build_label
-  
+
   def initialize(project)
     @activity = project.attributes["activity"]
     @last_build_time = Time.parse(project.attributes["lastBuildTime"]).localtime
