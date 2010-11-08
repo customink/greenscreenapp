cc97f69d9a8225508d94110a7de15e6003dce2cd
Rhett Sutphin <rhett@detailedbalance.net>
2010-11-08 10:28:49 -0600

    If loading a server's XML fails, report that that server is down on the display instead of completely failing to load.

--- greenscreen.rb
+++ greenscreen.rb
@@ -20,29 +20,11 @@ get '/' do
     if server["username"] || server["password"]
       open_opts[:http_basic_authentication] = [ server["username"], server["password"] ]
     end
-    xml = REXML::Document.new(open(server["url"], open_opts))
-    projects = xml.elements["//Projects"]
-
-    job_matchers =
-      if server["jobs"]
-        server["jobs"].collect do |j|
-          if j =~ %r{^/.*/$}
-            Regexp.new(j[1..(j.size-2)])
-          else
-            Regexp.new("^#{Regexp.escape(j)}$")
-          end
-        end
-      end
-
-    projects.each do |project|
-      monitored_project = MonitoredProject.new(project)
-      if job_matchers
-        if job_matchers.detect { |matcher| monitored_project.name =~ matcher }
-          @projects << monitored_project
-        end
-      else
-        @projects << monitored_project
-      end
+    begin
+      xml = REXML::Document.new(open(server["url"], open_opts))
+      @projects.push(*accumulate_projects(server, xml))
+    rescue => e
+      @projects.push(MonitoredProject.server_down(server, e))
     end
   end
 
@@ -62,16 +44,55 @@ def load_servers
   YAML.load(StringIO.new(ERB.new(File.read 'config.yml').result))
 end
 
-class MonitoredProject
-  attr_reader :name, :last_build_status, :activity, :last_build_time, :web_url, :last_build_label
+def accumulate_projects(server, xml)
+  projects = xml.elements["//Projects"]
 
-  def initialize(project)
-    @activity = project.attributes["activity"]
-    @last_build_time = Time.parse(project.attributes["lastBuildTime"]).localtime
-    @web_url = project.attributes["webUrl"]
-    @last_build_label = project.attributes["lastBuildLabel"]
-    @last_build_status = project.attributes["lastBuildStatus"].downcase
-    @name = project.attributes["name"]
+  job_matchers =
+    if server["jobs"]
+      server["jobs"].collect do |j|
+        if j =~ %r{^/.*/$}
+          Regexp.new(j[1..(j.size-2)])
+        else
+          Regexp.new("^#{Regexp.escape(j)}$")
+        end
+      end
+    end
+
+  projects.collect do |project|
+    monitored_project = MonitoredProject.create(project)
+    if job_matchers
+      if job_matchers.detect { |matcher| monitored_project.name =~ matcher }
+        monitored_project
+      end
+    else
+      monitored_project
+    end
+  end.flatten
+end
+
+class MonitoredProject
+  attr_accessor :name, :last_build_status, :activity, :last_build_time, :web_url, :last_build_label
+
+  def self.create(project)
+    MonitoredProject.new.tap do |mp|
+      mp.activity = project.attributes["activity"]
+      mp.last_build_time = Time.parse(project.attributes["lastBuildTime"]).localtime
+      mp.web_url = project.attributes["webUrl"]
+      mp.last_build_label = project.attributes["lastBuildLabel"]
+      mp.last_build_status = project.attributes["lastBuildStatus"].downcase
+      mp.name = project.attributes["name"]
+    end
+  end
+
+  def self.server_down(server, e)
+    MonitoredProject.new.tap do |mp|
+      mp.name = e.to_s
+      mp.last_build_time = Time.now.localtime
+      mp.last_build_label = server["url"]
+      mp.web_url = server["url"]
+      mp.last_build_status = "Failure"
+      mp.activity = "Sleeping"
+    end
   end
 
   def building?
