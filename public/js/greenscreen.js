6791fecf5450987dfb50ddc8eb7f74de544cbc34
Derek Lindahl <dlindahl@customink.com>
2011-03-28 10:12:47 -0400

    Added ISO8601 support for Safari.

--- public/js/greenscreen.js
+++ public/js/greenscreen.js
@@ -1,13 +1,20 @@
-$(function() {
-  $('time').each(function(i, el) {
-    $el = $(el);
-    var ts = new Date(el.getAttribute('datetime'));
-    $el.html( $el.time_ago_in_words_with_parsing( ts ) ).removeClass('invisible');
-  });
+yepnope([{
+  test : isNaN(new Date("2011-01-01T00:00:00-05:00").getTime()),
+  yep : ['/js/iso8601_support.js'],
+  complete : function() {
 
-  var $ok = $('#ok');
-  if($ok.length > 0) {
-    var w = $ok.width();
-    $ok.width( w > 1000 ? 1000 : w ).bigtext(); // bigtext() doesn't work very well with full-width elements
+      $('time').each(function(i, el) {
+        $el = $(el);
+
+        var ts = new Date(Date.parse(el.getAttribute('datetime')));
+        $el.html( $el.time_ago_in_words_with_parsing( ts ) ).removeClass('invisible');
+      });
+
+      var $ok = $('#ok');
+      if($ok.length > 0) {
+        var w = $ok.width();
+        $ok.width( w > 1000 ? 1000 : w ).bigtext(); // bigtext() doesn't work very well with full-width elements
+      }
+
   }
-});
+}]);
---
+++ public/js/iso8601_support.js
@@ -0,0 +1,26 @@
+/**
+ * Date.parse with progressive enhancement for ISO-8601, version 2
+ * © 2010 Colin Snover <http://zetafleet.com>
+ * Released under MIT license.
+ * From http://zetafleet.com/blog/javascript-dateparse-for-iso-8601
+ * Slightly modified to handle undefined struct[7] for the value "2011-01-01T00:00:00-05:00"
+ */
+;(function () {
+  var origParse = Date.parse;
+  Date.parse = function (date) {
+    var timestamp = origParse(date), minutesOffset = 0, struct;
+    if(isNaN(timestamp) && (struct = /^(\d{4}|[+\-]\d{6})-(\d{2})-(\d{2})(?:[T ](\d{2}):(\d{2})(?::(\d{2})(?:\.(\d{3,}))?)?(?:(Z)|([+\-])(\d{2})(?::?(\d{2}))?))?/.exec(date))) {
+      if(struct[8] !== 'Z') {
+        minutesOffset = +struct[10] * 60 + (+struct[11]);
+
+        if (struct[9] === '+') {
+          minutesOffset = 0 - minutesOffset;
+        }
+      }
+
+      timestamp = Date.UTC(+struct[1], +struct[2] - 1, +struct[3], +struct[4], +struct[5] + minutesOffset, +struct[6], +(struct[7]||'').substr(0, 3));
+    }
+
+    return timestamp;
+  };
+}());
